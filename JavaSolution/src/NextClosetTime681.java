a2894de8590f0814b7213c8bafb3ab0b68ae6266
Mokuten OH <mokuteno@gmail.com>
2017-12-04 11:45:44 -0800

    modified 681, using dfs to generate all possible times

--- JavaSolution/src/NextClosetTime681.java
+++ JavaSolution/src/NextClosetTime681.java
@@ -1,23 +1,51 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 class NextClosetTime681 {
-    static int[] mins = { 600, 60, 10, 1 };
-    public static String nextClosestTime(String time) {
-        int colon = time.indexOf(':');
-        int cur = Integer.valueOf(time.substring(0, colon)) * 60 + Integer.valueOf(time.substring(colon + 1));
-        char[] next = new char[4];
-        for (int i = 1, d = 0; i <= 1440 && d < 4; i++) {
-            int m = (cur + i) % 1440;
-            for (d = 0; d < 4; d++) {
-                next[d] = (char)('0' + m / mins[d]); m %= mins[d];
-                if (time.indexOf(next[d]) == -1) break;
+    int diff = Integer.MAX_VALUE;
+    String result = "";
+
+    public String nextClosestTime(String time) {
+        // 使用set来一次性去重
+        Set<Integer> set = new HashSet<>();
+        set.add(Integer.parseInt(time.substring(0, 1)));
+        set.add(Integer.parseInt(time.substring(1, 2)));
+        set.add(Integer.parseInt(time.substring(3, 4)));
+        set.add(Integer.parseInt(time.substring(4, 5)));
+
+        if (set.size() == 1) return time;
+
+        List<Integer> digits = new ArrayList<>(set);
+        int minute = Integer.parseInt(time.substring(0, 2)) * 60 + Integer.parseInt(time.substring(3, 5));
+
+        dfs(digits, "", 0, minute);
+
+        return result;
+    }
+    // 用dfs尝试所有排列。从""开始记录当前的index和在index之前的字符串，再从index位置上开始dfs（循环该位可选digit+递归至下一位）
+    private void dfs(List<Integer> digits, String cur, int pos, int target) {
+        // 当到最后一位时更新diff
+        if (pos == 4) {
+            int m = Integer.parseInt(cur.substring(0, 2)) * 60 + Integer.parseInt(cur.substring(2, 4));
+            if (m == target) return;
+            int d = m - target > 0 ? m - target : 1440 + m - target;
+            if (d < diff) {
+                diff = d;
+                result = cur.substring(0, 2) + ":" + cur.substring(2, 4);
             }
+            return;
         }
-        return new String(next, 0, 2) + ':' + new String(next, 2, 2);
+        // 在遍历每个数字时做合法check，跳过不合理的time
+        for (int i = 0; i < digits.size(); i++) {
+            if (pos == 0 && digits.get(i) > 2) continue;
+            if (pos == 1 && Integer.parseInt(cur) * 10 + digits.get(i) > 23) continue;
+            if (pos == 2 && digits.get(i) > 5) continue;
+            if (pos == 3 && Integer.parseInt(cur.substring(2)) * 10 + digits.get(i) > 59) continue;
+            dfs(digits, cur + digits.get(i), pos + 1, target);
+        }
     }
 
-    public static void main(String[] args)
-    {
-        String res = nextClosestTime("21:01");
-        System.out.println(res);
-    }
 
 }
---
+++ JavaSolution/src/TestEngine.java
@@ -0,0 +1,9 @@
+public class TestEngine {
+
+    public static void main(String[] args)
+    {
+        NextClosetTime681 solution = new NextClosetTime681();
+        String res = solution.nextClosestTime("21:01");
+        System.out.println(res);
+    }
+}
