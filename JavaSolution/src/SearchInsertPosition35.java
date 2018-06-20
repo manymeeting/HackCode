f5bcb1681c45c77072648f8467a134dd3f99ba11
Mokuten OH <mokuteno@gmail.com>
2018-06-19 23:18:36 -0700

    Search Insert Position

---
+++ JavaSolution/src/SearchInsertPosition35.java
@@ -0,0 +1,13 @@
+public class SearchInsertPosition35 {
+    public int searchInsert(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(target <= nums[i])
+            {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+}
