ffd90ff3db8ee9f15068c4df304292dc3d5b801d
Mokuten OH <mokuteno@gmail.com>
2018-07-15 00:19:53 -0700

    Ransom note 383

---
+++ JavaSolution/src/RansomNote383.java
@@ -0,0 +1,41 @@
+/**
+ * Given an arbitrary ransom note string and another string containing letters from all the magazines,
+ * write a function that will return true if the ransom note can be constructed from the magazines ;
+ * otherwise, it will return false.
+
+ Each letter in the magazine string can only be used once in your ransom note.
+
+ Note:
+ You may assume that both strings contain only lowercase letters.
+
+ canConstruct("a", "b") -> false
+ canConstruct("aa", "ab") -> false
+ canConstruct("aa", "aab") -> true
+
+ */
+
+public class RansomNote383 {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] arr = new int[26];
+
+
+        //按字母统计出现顺序
+        for(int i = 0; i < magazine.length(); i++)
+        {
+            arr[magazine.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i < ransomNote.length(); i++)
+        {
+            int checkCount = --arr[ransomNote.charAt(i) - 'a'];
+            if(checkCount < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
