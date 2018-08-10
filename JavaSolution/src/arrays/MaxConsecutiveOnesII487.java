cbeb9e1b9e998a101a1e799c4b99161d8a71aced
Mokuten OH <mokuteno@gmail.com>
2018-08-09 23:59:23 -0700

    MaxConsecutiveOnesII487

---
+++ JavaSolution/src/arrays/MaxConsecutiveOnesII487.java
@@ -0,0 +1,48 @@
+package arrays;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a binary array, find the maximum number of consecutive 1s in this array if you can flip at most one 0.
+
+ Example 1:
+ Input: [1,0,1,1,0]
+ Output: 4
+ Explanation: Flip the first zero will get the the maximum number of consecutive 1s.
+ After flipping, the maximum number of consecutive 1s is 4.
+ Note:
+
+ The input array will only contain 0 and 1.
+ The length of input array is a positive integer and will not exceed 10,000
+ Follow up:
+ What if the input numbers come in one by one as an infinite stream? In other words,
+ you can't store all numbers coming from the stream as it's too large to hold in memory.
+ Could you solve it efficiently?
+ */
+
+// 维护一个滑动窗口，[l, h] that contains at most maxFlipNumber zero
+
+public class MaxConsecutiveOnesII487 {
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        Queue<Integer> zeroIndices = new LinkedList<>();
+
+        int max = 0;
+        int maxFlipNumber = 1;
+        for(int l=0, h=0; h < nums.length; h++)
+        {
+            if(nums[h] == 0)
+            {
+                zeroIndices.offer(h);
+            }
+            if(zeroIndices.size() > maxFlipNumber)
+            {
+                l = zeroIndices.poll() + 1;
+            }
+            max = Math.max(max, h - l + 1);
+
+        }
+        return max;
+    }
+}
