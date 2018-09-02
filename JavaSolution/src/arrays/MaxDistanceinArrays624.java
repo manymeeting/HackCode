e078f602227fabce34dfc2f11c48652295fe1457
Mokuten OH <mokuteno@gmail.com>
2018-09-02 00:18:46 -0700

    Max Distance in Arrays 624

---
+++ JavaSolution/src/arrays/MaxDistanceinArrays624.java
@@ -0,0 +1,50 @@
+package arrays;
+
+import java.util.List;
+
+/**
+ * Given m arrays, and each array is sorted in ascending order.
+ * Now you can pick up two integers from two different arrays (each array picks one) and calculate the distance.
+ * We define the distance between two integers a and b to be their absolute difference |a-b|.
+ * Your task is to find the maximum distance.
+
+ Example 1:
+ Input:
+ [[1,2,3],
+ [4,5],
+ [1,2,3]]
+ Output: 4
+ Explanation:
+ One way to reach the maximum distance 4 is to pick 1 in the first or third array and pick 5 in the second array.
+ Note:
+ Each given array will have at least 1 number. There will be at least two non-empty arrays.
+ The total number of the integers in all the m arrays will be in the range of [2, 10000].
+ The integers in the m arrays will be in the range of [-10000, 10000].
+
+ */
+
+// 注意细节问题，比如用第一个list来初始化min和max，但遍历要从第二个开始
+public class MaxDistanceinArrays624 {
+
+    public int maxDistance(List<List<Integer>> arrays) {
+
+        int min = arrays.get(0).get(0);
+        int max = arrays.get(0).get(arrays.get(0).size()-1);
+
+        int res = Integer.MIN_VALUE;
+        for (int i = 1; i < arrays.size(); i++)
+        {
+            List<Integer> arr = arrays.get(i);
+            int currMin = arr.get(0);
+            int currMax = arr.get(arr.size()-1);
+            res = Math.max(res, Math.abs(currMax - min));
+            res = Math.max(res, Math.abs(max - currMin));
+
+            max = Math.max(max, currMax);
+            min = Math.min(min, currMin);
+
+        }
+
+        return res;
+    }
+}
