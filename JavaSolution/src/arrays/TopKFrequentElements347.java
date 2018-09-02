f54c58cd07ca10ca41bdd73778a148f533386655
Mokuten OH <mokuteno@gmail.com>
2018-09-01 22:19:21 -0700

    Top K Frequent Elements 347

---
+++ JavaSolution/src/arrays/TopKFrequentElements347.java
@@ -0,0 +1,57 @@
+package arrays;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *Given a non-empty array of integers, return the k most frequent elements.
+
+ Example 1:
+
+ Input: nums = [1,1,1,2,2,3], k = 2
+ Output: [1,2]
+ Example 2:
+
+ Input: nums = [1], k = 1
+ Output: [1]
+ Note:
+
+ You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+ Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ * */
+
+// 利用桶排序的思想，先build一个出现频率的map，然后建立一个list的array（buckets），每一项代表对应频率下的数字list，
+// 最后从尾部开始遍历buckets，往结果里放元素，直到个数>=k为止
+
+public class TopKFrequentElements347 {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+        // 1. build freq map
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums)
+        {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        // 2. build buckets on freq
+        List<Integer>[] buckets = new List[nums.length+1];
+        for (int i = 0; i < buckets.length; i++) buckets[i] = new ArrayList<>();
+        for (int key: freq.keySet())
+        {
+            buckets[freq.get(key)].add(key);
+        }
+
+        // 3. gather results
+        List<Integer> res = new ArrayList<>();
+        for (int i = buckets.length - 1; i >= 0; i--)
+        {
+            res.addAll(buckets[i]);
+            if(res.size() >= k) return res;
+        }
+
+        return res;
+    }
+
+}
