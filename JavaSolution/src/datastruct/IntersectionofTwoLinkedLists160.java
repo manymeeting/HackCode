52e08521582c5bdafa01d9a219a6e62d44c7b5a2
Mokuten OH <mokuteno@gmail.com>
2018-09-05 01:28:38 -0700

    Intersection of Two Linked Lists 160

---
+++ JavaSolution/src/datastruct/IntersectionofTwoLinkedLists160.java
@@ -0,0 +1,50 @@
+package datastruct;
+
+/**
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+ For example, the following two linked lists:
+
+ A:          a1 → a2
+ ↘
+ c1 → c2 → c3
+ ↗
+ B:     b1 → b2 → b3
+ begin to intersect at node c1.
+
+
+ Notes:
+
+ If the two linked lists have no intersection at all, return null.
+ The linked lists must retain their original structure after the function returns.
+ You may assume there are no cycles anywhere in the entire linked structure.
+ Your code should preferably run in O(n) time and use only O(1) memory.
+ Credits:
+ Special thanks to @stellari for adding this problem and creating all test cases.
+
+
+ */
+
+// very tricky:不check各自的长度，到达末尾后切换指针到另一个head，可以起到同步的效果（最多2个loop就能找到）
+
+public class IntersectionofTwoLinkedLists160 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        ListNode a = headA;
+        ListNode b = headB;
+        while(a != b)
+        {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return a;
+    }
+
+}
