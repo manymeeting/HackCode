97aff9b3e94e5fe484f3c711725352dfd069cd75
Mokuten OH <mokuteno@gmail.com>
2018-09-07 13:15:37 -0700

    Target Sum 494

---
+++ JavaSolution/src/others/TargetSum494.java
@@ -0,0 +1,50 @@
+package others;
+
+/**
+ * You are given a list of non-negative integers, a1, a2, ..., an, and a target, S.
+ * Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+
+ Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+ Example 1:
+ Input: nums is [1, 1, 1, 1, 1], S is 3.
+ Output: 5
+ Explanation:
+
+ -1+1+1+1+1 = 3
+ +1-1+1+1+1 = 3
+ +1+1-1+1+1 = 3
+ +1+1+1-1+1 = 3
+ +1+1+1+1-1 = 3
+
+ There are 5 ways to assign symbols to make the sum of nums be target 3.
+ Note:
+ The length of the given array is positive and will not exceed 20.
+ The sum of elements in the given array will not exceed 1000.
+ Your output answer is guaranteed to be fitted in a 32-bit integer.
+
+ */
+
+// dfs遍历，dfs(+num[i]) + dfs(-nums[i])
+
+public class TargetSum494 {
+
+    public int findTargetSumWays(int[] nums, int S) {
+        return dfs(nums, 0, S, 0);
+    }
+
+    private int dfs(int[] nums, int sum, int target, int index)
+    {
+        if(nums.length == index)
+        {
+            return sum == target ? 1 : 0;
+        }
+
+        return dfs(nums, sum + nums[index], target, index+1) +
+                dfs(nums, sum - nums[index], target, index+1);
+
+    }
+
+
+
+}
