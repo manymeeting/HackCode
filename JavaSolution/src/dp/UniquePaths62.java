1441077c16cd98d02a3799420ea14b7035a6e259
Mokuten OH <mokuteno@gmail.com>
2018-09-14 16:19:19 -0700

    62. Unique Paths

---
+++ JavaSolution/src/dp/UniquePaths62.java
@@ -0,0 +1,54 @@
+package dp;
+
+/**
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+ The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+ How many possible unique paths are there?
+
+
+ Note: m and n will be at most 100.
+
+ Example 1:
+
+ Input: m = 3, n = 2
+ Output: 3
+ Explanation:
+ From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+ 1. Right -> Right -> Down
+ 2. Right -> Down -> Right
+ 3. Down -> Right -> Right
+ Example 2:
+
+ Input: m = 7, n = 3
+ Output: 28
+
+ */
+
+// DP问题，suppose the number of paths to arrive at a point (i, j) is denoted as P[i][j],
+// it is easily concluded that P[i][j] = P[i - 1][j] + P[i][j - 1].
+
+
+public class UniquePaths62 {
+    public int uniquePaths(int m, int n) {
+        int[][] matrix = new int[m][n];
+        for (int i = 0;i < m; i++)
+        {
+            matrix[][0] = 1;
+        }
+        for (int j = 0; j < n; j++)
+        {
+            matrix[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
+            }
+        }
+
+        return matrix[m-1][n-1];
+    }i
+}
