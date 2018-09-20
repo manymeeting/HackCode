eae5ef4895ea352b8d7b2f2b310f40ecfae92e6e
Mokuten OH <mokuteno@gmail.com>
2018-09-20 14:25:27 -0700

    153. Find Minimum in Rotated Sorted Array

---
+++ JavaSolution/src/arrays/FindMininRotatedSortedArr153.java
@@ -0,0 +1,40 @@
+package arrays;
+
+/**
+ *
+ Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+ (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+ Find the minimum element.
+
+ You may assume no duplicate exists in the array.
+
+ Example 1:
+
+ Input: [3,4,5,1,2]
+ Output: 1
+ Example 2:
+
+ Input: [4,5,6,7,0,1,2]
+ Output: 0
+
+ * */
+
+// 二分查找，找到num[mid] >= num[right]的点，然后最小值肯定在该点右侧，除此之外窗口向左逼近
+public class FindMininRotatedSortedArr153 {
+    public int findMin(int[] nums) {
+
+        int left = 0, right = nums.length-1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] >= nums[right]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
