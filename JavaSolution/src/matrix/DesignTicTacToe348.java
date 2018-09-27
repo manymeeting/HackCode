9b9aaccb3003f11170a527e79b71e0f4c9a068b0
Mokuten OH <mokuteno@gmail.com>
2018-09-27 10:57:52 -0700

    348. Design Tic-Tac-Toe

---
+++ JavaSolution/src/matrix/DesignTicTacToe348.java
@@ -0,0 +1,106 @@
+package matrix;
+
+/**
+ * Design a Tic-tac-toe game that is played between two players on a n x n grid.
+
+ You may assume the following rules:
+
+ A move is guaranteed to be valid and is placed on an empty block.
+ Once a winning condition is reached, no more moves is allowed.
+ A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+ Example:
+ Given n = 3, assume that player 1 is "X" and player 2 is "O" in the board.
+
+ TicTacToe toe = new TicTacToe(3);
+
+ toe.move(0, 0, 1); -> Returns 0 (no one wins)
+ |X| | |
+ | | | |    // Player 1 makes a move at (0, 0).
+ | | | |
+
+ toe.move(0, 2, 2); -> Returns 0 (no one wins)
+ |X| |O|
+ | | | |    // Player 2 makes a move at (0, 2).
+ | | | |
+
+ toe.move(2, 2, 1); -> Returns 0 (no one wins)
+ |X| |O|
+ | | | |    // Player 1 makes a move at (2, 2).
+ | | |X|
+
+ toe.move(1, 1, 2); -> Returns 0 (no one wins)
+ |X| |O|
+ | |O| |    // Player 2 makes a move at (1, 1).
+ | | |X|
+
+ toe.move(2, 0, 1); -> Returns 0 (no one wins)
+ |X| |O|
+ | |O| |    // Player 1 makes a move at (2, 0).
+ |X| |X|
+
+ toe.move(1, 0, 2); -> Returns 0 (no one wins)
+ |X| |O|
+ |O|O| |    // Player 2 makes a move at (1, 0).
+ |X| |X|
+
+ toe.move(2, 1, 1); -> Returns 1 (player 1 wins)
+ |X| |O|
+ |O|O| |    // Player 1 makes a move at (2, 1).
+ |X|X|X|
+ Follow up:
+ Could you do better than O(n2) per move() operation?
+
+ */
+
+// 用四个变量来继续，两个arr分别记录row和col的情况，对角线和反对角线各一个，
+// 遇到player1就+1，遇到另一个player就-1，每次看是否有任何一个变量的绝对值达到len，达到则直接返回当前player
+public class DesignTicTacToe348 {
+
+    private int[] cols;
+    private int[] rows;
+    private int diagonal;
+    private int antiDiagonal;
+
+    /** Initialize your data structure here. */
+    public DesignTicTacToe348(int n) {
+        cols = new int[n];
+        rows = new int[n];
+    }
+
+    /** Player {player} makes a move at ({row}, {col}).
+     @param row The row of the board.
+     @param col The column of the board.
+     @param player The player, can be either 1 or 2.
+     @return The current winning condition, can be either:
+     0: No one wins.
+     1: Player 1 wins.
+     2: Player 2 wins. */
+    public int move(int row, int col, int player) {
+
+        int add = player == 1 ? 1 : -1;
+
+        int len = cols.length;
+
+        cols[col]+=add;
+        rows[row]+=add;
+        if(row == col) {
+            diagonal+=add;
+        }
+        if(row + col + 1 == len) {
+            antiDiagonal+=add;
+        }
+
+        if(Math.abs(cols[col]) == len || Math.abs(rows[row]) == len
+                || Math.abs(diagonal) == len || Math.abs(antiDiagonal) == len) {
+            return player;
+        }
+
+        return 0;
+    }
+}
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
