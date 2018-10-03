d40b171240183ea578a50ef3123b2c772d5750ba
Mokuten OH <mokuteno@gmail.com>
2018-10-02 17:30:13 -0700

    278. First Bad Version

---
+++ JavaSolution/src/others/FirstBadVersion278.java
@@ -0,0 +1,47 @@
+package others;
+
+/**
+ * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+ Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+ You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+ Example:
+
+ Given n = 5, and version = 4 is the first bad version.
+
+ call isBadVersion(3) -> false
+ call isBadVersion(5) -> true
+ call isBadVersion(4) -> true
+
+ Then 4 is the first bad version.
+
+ */
+
+
+// 基本的二分查找，注意start和end的初始值，直接设为1和n
+    
+public class FirstBadVersion278 {
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+    // Dummy api
+    private boolean isBadVersion(int version) {
+        return false;
+    }
+
+    public int firstBadVersion(int n) {
+        int start = 1, end = n;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(!isBadVersion(mid)) {
+                start = mid + 1;
+            }
+            else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
