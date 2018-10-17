f8571bff747264e197c14b87b2e8108ca248d942
Mokuten OH <mokuteno@gmail.com>
2018-10-17 01:15:06 -0700

    496. Next Greater Element I

---
+++ JavaSolution/src/arrays/NextGreaterElementI496.java
@@ -0,0 +1,63 @@
+package arrays;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+
+ The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+
+ Example 1:
+ Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ Output: [-1,3,-1]
+ Explanation:
+ For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+ For number 1 in the first array, the next greater number for it in the second array is 3.
+ For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+
+ Example 2:
+ Input: nums1 = [2,4], nums2 = [1,2,3,4].
+ Output: [3,-1]
+ Explanation:
+ For number 2 in the first array, the next greater number for it in the second array is 3.
+ For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+
+ Note:
+ All elements in nums1 and nums2 are unique.
+ The length of both nums1 and nums2 would not exceed 1000.
+ */
+
+
+// 思路：用一个stack和一个map，先遍历nums2，
+// 遇到当前num > stack顶端数字时，一个个pop出来并且map.put(stack.pop(), num);
+// 然后遍历nums1，从map里一个个get，没有key的话设为-1即可
+/**
+ * 思路举例：For example [9, 8, 7, 3, 2, 1, 6]
+ * The stack will first contain [9, 8, 7, 3, 2, 1]
+ * and then we see 6 which is greater than 1 so we pop 1 2 3 whose next greater element should be 6
+ */
+public class NextGreaterElementI496 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums2) {
+            while(!stack.isEmpty() && num > stack.peek()) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return res;
+    }
+
+}
