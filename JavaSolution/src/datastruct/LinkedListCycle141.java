0fdd1293e2da2d320c757be29b325d872c6c488a
Mokuten OH <mokuteno@gmail.com>
2018-10-28 19:00:34 -0700

    141. Linked List Cycle

---
+++ JavaSolution/src/datastruct/LinkedListCycle141.java
@@ -0,0 +1,36 @@
+package datastruct;
+
+/**
+ *
+ Given a linked list, determine if it has a cycle in it.
+
+ Follow up:
+ Can you solve it without using extra space?
+
+ */
+
+// 用fast和slow来解决，注意while循环的空值判断
+
+public class LinkedListCycle141 {
+    class ListNode {
+         int val;
+         ListNode next;
+         ListNode(int x) {
+             val = x;
+             next = null;
+         }
+    }
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) return false;
+
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast != null && slow != null && fast.next != null) {
+            if(fast == slow) return true;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return false;
+    }
+}
