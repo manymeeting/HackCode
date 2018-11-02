02f7d32a71d2ff85a5f197c5fb5c0ca8de0b9352
Mokuten OH <mokuteno@gmail.com>
2018-11-01 21:27:03 -0700

    271. Encode and Decode Strings

---
+++ JavaSolution/src/datastruct/EncodeandDecodeStr271.java
@@ -0,0 +1,70 @@
+package datastruct;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+ Machine 1 (sender) has the function:
+
+ string encode(vector<string> strs) {
+ // ... your code
+ return encoded_string;
+ }
+ Machine 2 (receiver) has the function:
+ vector<string> decode(string s) {
+ //... your code
+ return strs;
+ }
+ So Machine 1 does:
+
+ string encoded_string = encode(strs);
+ and Machine 2 does:
+
+ vector<string> strs2 = decode(encoded_string);
+ strs2 in Machine 2 should be the same as strs in Machine 1.
+
+ Implement the encode and decode methods.
+
+ Note:
+ The string may contain any possible characters out of 256 valid ascii characters. Your algorithm should be generalized enough to work on any possible characters.
+ Do not use class member/global/static variables to store states. Your encode and decode algorithms should be stateless.
+ Do not rely on any library method such as eval or serialize methods. You should implement your own encode/decode algorithm.
+
+ */
+
+
+// 用<len>/原string来表示，即使原str中有/也没关系，因为会先解析当前第一个/左边的len，然后按照len来读取原str
+// decode时每次根据len来移动读取位置
+// 注意indexOf的第二个参数的使用，fromIndex
+
+public class EncodeandDecodeStr271 {
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str.length()).append("/").append(str);
+        }
+
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        while (i < s.length()) {
+            int splitterIdx = s.indexOf("/", i);
+            int strLen = Integer.parseInt(s.substring(i, splitterIdx));
+            res.add(s.substring(splitterIdx + 1, splitterIdx + strLen + 1));
+
+            i = splitterIdx + strLen + 1;
+        }
+        return res;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
