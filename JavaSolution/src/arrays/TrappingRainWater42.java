6cc41610b9f9542fda70ad0b7e8b32445affcbae
Mokuten OH <mokuteno@gmail.com>
2018-11-04 02:42:52 -0800

    update solution

--- JavaSolution/src/arrays/TrappingRainWater42.java
+++ JavaSolution/src/arrays/TrappingRainWater42.java
@@ -14,13 +14,46 @@ package arrays;
 
  */
 
-// 2pointer，while循环遍历，可以想象成两个pointer是容器的两个边，每次按短边一侧的max来计算容量，
-// followup：如果有漏水的位置
+// 2-pass构造两个数组，分别存从左到右每个位置左边的max和从右到左每个位置右边的max，
+// 最后遍历原数组，每个位置取左右max的最小值和当前高度相减，如果是正数则累加
+
+// followup：如果有漏水的位置，把对应位置的左右任一个max设为0即可
 
 public class TrappingRainWater42 {
+
+    // 2-pass方法（优点：可以方便处理漏水的followup）
+    public int trap(int[] height) {
+        int len = height.length;
+        if(len == 0) return 0;
+        int[] leftMaxArr = new int[len];
+        int[] rightMaxArr = new int[len];
+
+        leftMaxArr[0] = height[0];
+        rightMaxArr[len-1] = height[len-1];
+
+        for (int i = 1; i < len; i++) {
+            leftMaxArr[i] = Math.max(leftMaxArr[i-1], height[i]);
+        }
+        for (int i = len-2; i >= 0; i--) {
+            rightMaxArr[i] = Math.max(rightMaxArr[i+1], height[i]);
+        }
+
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            int diff = Math.min(leftMaxArr[i], rightMaxArr[i]) - height[i];
+            res += diff > 0 ? diff : 0;
+        }
+
+        return res;
+    }
+
+
+
+    /* 双指针solution：while循环遍历，可以想象成两个pointer是容器的两个边，每次按短边一侧的max来计算容量，
+
     public int trap(int[] height) {
         int a = 0;
-        int b = height.length - 1;
+        int b = len - 1;
 
         int maxWater = 0;
         int leftMax = 0;
@@ -44,4 +77,6 @@ public class TrappingRainWater42 {
         }
         return maxWater;
     }
+    */
+
 }
