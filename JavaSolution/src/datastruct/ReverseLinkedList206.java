b75db5e5128e7a1b9db924f5f63a433de4236e63
Mokuten OH <mokuteno@gmail.com>
2018-11-07 09:44:37 -0500

    206. Reverse Linked List

---
+++ JavaSolution/src/datastruct/ReverseLinkedList206.java
@@ -0,0 +1,35 @@
+package datastruct;
+
+/**
+ * Reverse a singly linked list.
+
+ Example:
+
+ Input: 1->2->3->4->5->NULL
+ Output: 5->4->3->2->1->NULL
+ Follow up:
+
+ A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+
+// 递归解法：每次递归遇到null或者next为null，直接返回，
+// 否则交换当前node和nextNode，并返回用nextNode递归之后的节点
+    
+public class ReverseLinkedList206 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode nextNode = head.next;
+        ListNode newHead = reverseList(nextNode);
+        nextNode.next = head; // 此处做reverse
+        head.next = null;
+        return newHead; // 最后会返回原list的最后一个节点
+    }
+}
