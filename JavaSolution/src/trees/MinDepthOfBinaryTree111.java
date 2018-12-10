0d7676e6d25ac8a3f6cd60decf23f00533ce9b12
Mutian Wang <mokuteno@gmail.com>
2018-12-09 18:54:21 -0800

    111. Minimum Depth of Binary Tree

---
+++ JavaSolution/src/trees/MinDepthOfBinaryTree111.java
@@ -0,0 +1,61 @@
+package trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, find its minimum depth.
+
+ The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+ Note: A leaf is a node with no children.
+
+ Example:
+
+ Given binary tree [3,9,20,null,null,15,7],
+
+ 3
+ / \
+ 9  20
+ /  \
+ 15   7
+ return its minimum depth = 2.
+ */
+
+// BFS遍历，每层走完后depth+1，遇到lead节点直接return
+
+public class MinDepthOfBinaryTree111 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        int depth = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            depth++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                if(curr.left == null && curr.right == null) {
+                    return depth;
+                }
+
+                if(curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if(curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+
+        }
+
+        return depth;
+    }
+}
