db321b83d85e2d34d242e4641322c7726cd17a5e
Mutian Wang <mokuteno@gmail.com>
2019-01-18 21:39:35 -0800

    771. Jewels and Stones

---
+++ JavaSolution/src/strings/JewelsAndStones771.java
@@ -0,0 +1,40 @@
+package strings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+ The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ Example 1:
+
+ Input: J = "aA", S = "aAAbbbb"
+ Output: 3
+ Example 2:
+
+ Input: J = "z", S = "ZZ"
+ Output: 0
+ Note:
+
+ S and J will consist of letters and have length at most 50.
+ The characters in J are distinct.
+ */
+
+// 用Set存jewels
+public class JewelsAndStones771 {
+    public int numJewelsInStones(String J, String S) {
+        Set<Character> jewels = new HashSet<>();
+        for (char ch : J.toCharArray()) {
+            jewels.add(ch);
+        }
+
+        int count = 0;
+        for (char ch : S.toCharArray()) {
+            if(jewels.contains(ch)) count++;
+        }
+
+        return count;
+    }
+}
