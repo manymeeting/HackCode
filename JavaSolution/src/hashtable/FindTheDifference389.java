898d8b1659bfee36e471af361efcb2be0030bac7
Mutian Wang <mokuteno@gmail.com>
2019-02-05 16:40:35 -0800

    Reorganize packages

--- JavaSolution/src/hashtable/FindTheDifference389.java
+++ JavaSolution/src/hashtable/FindTheDifference389.java
@@ -1,4 +1,4 @@
-package strings;
+package hashtable;
 
 import java.util.HashMap;
 import java.util.Map;
--- JavaSolution/src/hashtable/RepeatedDNASeq187.java
+++ JavaSolution/src/hashtable/RepeatedDNASeq187.java
@@ -1,4 +1,4 @@
-package strings;
+package hashtable;
 
 import java.util.ArrayList;
 import java.util.HashSet;
