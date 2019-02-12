bb64547b4b5bdae03a707d737d58efbe3f6b8c98
Mutian Wang <mokuteno@gmail.com>
2019-02-12 13:31:48 -0800

    5. Longest Palindromic Substring

---
+++ JavaSolution/src/strings/LongestPalindromicSubstring5.java
@@ -0,0 +1,31 @@
+package strings;
+
+
+// 遍历每个index，每次都尝试odd len和even len的palindrome，用全局变量记录最大值
+public class LongestPalindromicSubstring5 {
+    private int lo, maxLen;
+
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2)
+            return s;
+
+        for (int i = 0; i < len-1; i++) {
+            extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+            extendPalindrome(s, i, i+1); //assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+
+}
