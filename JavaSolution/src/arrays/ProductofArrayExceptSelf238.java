9730ec7c289f8142ba0d0a3c46e42a4d2901aec7
Mutian Wang <mokuteno@gmail.com>
2019-02-12 13:23:43 -0800

    Update comments

--- JavaSolution/src/arrays/ProductofArrayExceptSelf238.java
+++ JavaSolution/src/arrays/ProductofArrayExceptSelf238.java
@@ -18,7 +18,12 @@ import java.util.Arrays;
 
  */
 
-// 新建一个res数组（题目允许），res[0]=1，第一遍从左往右，逐一相乘，第二遍从右往左，逐一相乘
+// 解法一，两个数组：对于某一个数字，如果我们知道其前面所有数字的乘积，同时也知道后面所有的数乘积，那么二者相乘就是我们要的结果
+
+// 解法二，用一个数组：对上面的方法进行空间上的优化，由于最终的结果都是要乘到结果res中，所以我们可以不用单独的数组来保存乘积，而是直接累积到res中，
+// 我们先从前面遍历一遍，将乘积的累积存入res中，然后从后面开始遍历，用到一个临时变量right，初始化为1，然后每次不断累积，最终得到正确结果
+
+// 解释：http://www.cnblogs.com/grandyang/p/4650187.html
 public class ProductofArrayExceptSelf238 {
     public int[] productExceptSelf(int[] nums) {
         int len = nums.length;
