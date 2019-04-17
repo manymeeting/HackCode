c01f3f4f62bdf38d6daa52e6022fd82ca74a333e
Mutian Wang <mokuteno@gmail.com>
2019-04-17 00:15:39 -0700

    Update comments

--- JavaSolution/src/trees/DeleteNodeInBST450.java
+++ JavaSolution/src/trees/DeleteNodeInBST450.java
@@ -3,40 +3,40 @@ package trees;
 /**
  * Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
 
- Basically, the deletion can be divided into two stages:
+Basically, the deletion can be divided into two stages:
 
- Search for a node to remove.
- If the node is found, delete the node.
- Note: Time complexity should be O(height of tree).
+Search for a node to remove.
+If the node is found, delete the node.
+Note: Time complexity should be O(height of tree).
 
- Example:
+Example:
 
- root = [5,3,6,2,4,null,7]
- key = 3
+root = [5,3,6,2,4,null,7]
+key = 3
 
- 5
- / \
- 3   6
+    5
+   / \
+  3   6
  / \   \
- 2   4   7
+2   4   7
 
- Given key to delete is 3. So we find the node with value 3 and delete it.
+Given key to delete is 3. So we find the node with value 3 and delete it.
 
- One valid answer is [5,4,6,2,null,null,7], shown in the following BST.
+One valid answer is [5,4,6,2,null,null,7], shown in the following BST.
 
- 5
- / \
- 4   6
+    5
+   / \
+  4   6
  /     \
- 2       7
+2       7
 
- Another valid answer is [5,2,6,null,4,null,7].
+Another valid answer is [5,2,6,null,4,null,7].
 
- 5
- / \
- 2   6
- \   \
- 4   7
+    5
+   / \
+  2   6
+   \   \
+    4   7
  */
 
 // 找到目标node后，如果目标节点有左右children，则找出右子树中最小的（最左下方），作为新root
@@ -54,18 +54,18 @@ public class DeleteNodeInBST450 {
         if(root.val < key) root.right = deleteNode(root.right, key);
         else if(root.val > key) root.left = deleteNode(root.left, key);
         else {
-            // found the target
-            if(root.left == null) return root.right;
-            else if(root.right == null) return root.left;
-            else {
+            // root.val == val
+            if(root.left == null) return root.right; // 只有一个child
+            else if(root.right == null) return root.left; // 只有一个child
+            else { // 有两个children
                 TreeNode newRoot = root.right;
                 TreeNode parent = null;
-                // 找出这部分树中最小的作为new root
+                // 找出右子树中最左边的作为new root
                 while(newRoot.left != null) {
                     parent = newRoot;
                     newRoot = newRoot.left;
                 }
-                if(parent == null) {
+                if(parent == null) { // 注意检查parent是否为null，是的话说明root.right就是右边最小的
                     newRoot.left = root.left;
                     return newRoot;
                 }
